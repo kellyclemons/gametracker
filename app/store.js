75171d9a8a819781a816b49da5c152de5521c8bb
Kelly Clemons <kellyclemons@kellys-air.attlocal.net>
2017-03-06 20:46:52 -0600

    set up default state test

--- app/store.js
+++ app/store.js
@@ -4,7 +4,19 @@ import thunk from 'redux-thunk';
 import reducer from './reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const initialState = {  };
+const initialState = {
+  loading: false,
+  showOnlyAt: null, // 'home', 'campus.
+  games: [
+    {
+      _id: '1234',
+      name: 'Betrayal',
+      minPlayerCount: 3,
+      maxPlayerCount: 5,
+      atCampus: true
+    }
+  ],
+};
 
 const store = createStore(
   reducer,
